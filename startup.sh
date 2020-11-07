3440b58debab98e139c72a6dee2fb2d4cd358b61
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-11-07 15:38:51 +0000

    startup fixes

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'setting up environment'
-eval `ssh-agent -s`
+eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 source ~/captcha_env/bin/activate
 cd ~/solver/scalable-computing-project-2-pi/
@@ -9,26 +9,63 @@ cd ~/solver/scalable-computing-project-2-pi/
 echo 'attempting to check for and apply updates (timeout 60 seconds)'
 timeout 60 git pull
 exitstatus=$?
-if [$exitstatus == 124]
-then
-    echo 'failed to get updates, continuing with pre-existing code'
-else
-    echo 'updates pulled successfully'
+case $exitstatus in
+    124)
+        echo $exitstatus 'failed to get updates, continuing with pre-existing code'
+        ;;
+    125)
+        echo $exitstatus 'timeout command failed'
+        ;;
+    126)
+        echo $exitstatus 'git command cannot be invoked'
+        ;;
+    127)
+        echo $exitstatus 'git command not found'
+        ;;
+    137)
+        echo $exitstatus 'git command sent kill signal'
+        ;;
+    0)
+        echo $exitstatus 'updates pulled successfully'
+        ;;
+    *)
+        echo $exitstatus 'unknown error'
+        ;;
+esac
 
 echo 'attempting to update / install needed packages (timeout 300 seconds)'
-timeout 300 pip3 install -r requirements.txt
+timeout 300 pip3 install --update -r requirements.txt
 exitstatus=$?
-if [$exitstatus == 124]
-then
-    echo 'failed to install / update packages, this may cause classification to fail'
-else
-    echo 'packages installed and updated successfully'
+case $exitstatus in
+    124)
+        echo $exitstatus 'failed to install / update packages, this may cause classification to fail'
+        ;;
+    125)
+        echo $exitstatus 'timeout command failed'
+        ;;
+    126)
+        echo $exitstatus 'pip3 command cannot be invoked'
+        ;;
+    127)
+        echo $exitstatus 'pip3 command not found'
+        ;;
+    137)
+        echo $exitstatus 'pip3 command sent kill signal'
+        ;;
+    0)
+        echo $exitstatus 'packages installed / updated successfully'
+        ;;
+    *)
+        echo $exitstatus 'unknown error'
+        ;;
+esac
 
 echo 'Running classification'
-python3 classify.py ...
+python3 classify.py --model-name model/model_2 --captcha-dir in/temp/ --output out/model_2_output.txt --symbols model/symbols.txt --captcha-len 5 --processes 4
 exitstatus=$?
 if [$exitstatus == 0]
 then
-    echo 'classification ended successfully'
+    echo $exitstatus 'classification ended successfully'
 else
-    echo 'classification failed'
+    echo $exitstatus 'classification failed'
+fi
